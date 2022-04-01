c71c127d586fa93f4714ac48a751c3337fdf0e4b
Sandra Karlsson <sakq21@student.bth.se>
2022-04-01 08:28:04 +0100

    finished kmom01

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReportController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/about")
+     */
+    public function about(): Response
+    {
+
+        return $this->render('about.html.twig');
+    }
+
+    /**
+     * @Route("/report")
+     */
+    public function report(): Response
+    {
+
+        return $this->render('report.html.twig');
+    }
+}
